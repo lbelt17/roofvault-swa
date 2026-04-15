b266db9315121a50e69e6ed2e81b92d536c8ebe5
lbelt17 <lbelt17@outlook.com>
2026-04-15 14:21:33 -0400

    lock admin access to lbelt17 only

--- api/_helpers/owners.js
+++ api/_helpers/owners.js
@@ -1,7 +1,4 @@
 const OWNER_EMAILS = [
-  "msaulnier@cncflorida.com",
-  "joesorentino10@gmail.com",
-  "jbelt@beltengineering.com",
   "lbelt17@outlook.com",
 ].map(function (e) { return e.toLowerCase(); });
 
